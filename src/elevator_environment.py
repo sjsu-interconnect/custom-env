08e21f1e99f3d2513869aec6f8402940407f14c2
johnwang21 <johnwang21@gmail.com>
2023-12-11 20:38:51 -0800

    Update elevator_environment.py

--- src/elevator_environment.py
+++ src/elevator_environment.py
@@ -42,8 +42,67 @@ class ElevatorEnv(gym.Env):
     def reset(self):
         pass
 
-    def step(self):
-        pass
+    def step(self, action):
+        # Make the changes in action
+        if action == 0:
+            # Move the elevator down one if possible
+            if self._position > 0:
+                self._position -= 1
+        elif action == 2:
+            # Move the elevator up one if possible
+            if self._position < self.num_floors - 1:
+                self._position += 1
+        elif action == 1:
+            # Everyone inside gets out, everyone outside goes in
+            # Update observation
+            self._floors_selected[self._position] = 0
+            self._floors_waiting[self._position] = 0
+            self._num_people -= self._people_inside[self._position]
+            self._num_people += self._people_waiting[self._position]
+
+            # Update invisible information
+            people_entered = self._people_waiting[self._position]
+            self._people_waiting[self._position] = 0
+            self._people_inside[self._position] = 0
+
+            # Generate floor information for new people entering the elevator
+            if self._position == 0:
+                # Goes to a random floor above
+                new_floors = self.np_random.integers(1, self.num_floors, size=people_entered)
+                for floor in new_floors:
+                    self._people_inside[floor] += 1
+            else:
+                # Goes to the first floor
+                self._people_inside[0] += people_entered
+        else:
+            # Do nothing, invalid action
+            print(f'Warning, invalid action taken: {action}')
+
+        # Calculate the reward
+        # -0.5 reward for everyone inside the elevator, -1 reward for everyone waiting for the elevator
+        reward = -sum(self._people_inside) * 0.5 - sum(self._people_waiting)
+
+        # Generate new people at every floor
+        new_people = self.np_random.integers(1, 5)
+        new_people_floors = self.np_random.integers(0, self.num_floors, size=new_people)
+        for new_people_floor in new_people_floors:
+            self._people_waiting[new_people_floor] += 1
+
+        # Update what is seen by the elevator
+        for index, num_people in enumerate(self._people_inside):
+            self._floors_selected[index] = int(num_people > 0)
+
+        for index, num_people in enumerate(self._people_waiting):
+            self._floors_waiting[index] = int(num_people > 0)
+
+        # Return observation and info
+        self.num_timesteps += 1
+        observation = self._get_obs()
+        info = self._get_info()
+        terminated = False  # Never ends
+        truncated = (self.num_timesteps < self.max_timesteps)  # Max timesteps
+
+        return observation, reward, terminated, truncated, info
 
     def close(self):
         pass

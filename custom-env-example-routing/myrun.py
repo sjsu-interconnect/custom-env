7f361e24a665856c2a7dede9c6ef2e9da72c64a0
genyajoe <genya.ishigaki@sjsu.edu>
2024-05-02 15:29:30 -0700

    updated the params to avoid warnings

--- custom-env-example-routing/myrun.py
+++ custom-env-example-routing/myrun.py
@@ -1,29 +1,46 @@
+# RL algorithm
 from ray.rllib.algorithms.ppo import PPOConfig
+import ray
+# to use a custom env
 from ray.tune.registry import register_env
 
+# my custom env
 from net_env import NetworkEnv
-
 import numpy as np
 
+# Just to suppress
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+ray.init()
+
+# registering my custom env with a name "netenv-v0" 
 def env_creator(env_config):
     return NetworkEnv()
 
-register_env("netenv-v0", env_creator)
+register_env('netenv-v0', env_creator)
 
-config = PPOConfig()
-config = config.rollouts(num_rollout_workers=1)
-config = config.training(gamma=0.999, lr=0.001)
-algo = config.build(env='netenv-v0')
 
-config = config.training(gamma=0.999, lr=0)
-baselinealgo = config.build(env='netenv-v0')
+# Set up RL 
+config = (PPOConfig()
+          .training(gamma=0.999, lr=0.001)
+          .environment(env='netenv-v0')
+          .resources(num_gpus=0)
+          .env_runners(num_env_runners=0, num_envs_per_env_runner=1)
+        )
 
-num_train_iter = 10
+algo = config.build()
 
-for _ in range(num_train_iter):
-    pporets = algo.train()['hist_stats']['episode_reward']
-    baserets = baselinealgo.train()['hist_stats']['episode_reward']
 
-print('------')
-print(f'Average return (100 eps) - PPO: {np.mean(pporets[:-100])}')
-print(f'Average return (100 eps) - baseline: {np.mean(baserets[:-100])}')
+blconfig = (PPOConfig()
+          .training(gamma=0.999, lr=0.0)
+          .environment(env='netenv-v0')
+          .resources(num_gpus=0)
+          .env_runners(num_env_runners=0, num_envs_per_env_runner=1)
+        )
+
+baseline = blconfig.build()
+
+for _ in range(10):
+    algo.train()
+    baseline.train()
